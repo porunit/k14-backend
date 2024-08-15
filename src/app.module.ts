a2f0fcc1f8caa3a7ffbcbb5ba556a54cf8efc3fa
Maksim Zakharov <max89701@gmail.com>
2024-08-15 07:09:25 +0300

    команды

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,36 +1,37 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { Telegraf } from "telegraf";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Telegraf } from 'telegraf';
 
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService,
+  providers: [
+    AppService,
     {
-      provide: "Telegraf",
+      provide: 'Telegraf',
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => {
-        const bot = new Telegraf(configService.get<string>("TELEGRAM_TOKEN"), {
-          handlerTimeout: Infinity
+        const bot = new Telegraf(configService.get<string>('TELEGRAM_TOKEN'), {
+          handlerTimeout: Infinity,
         });
 
-        bot.on("message", async function(ctx: any, next) {
-
+        bot.on('message', async function (ctx: any, next) {
           const prompt = ctx.update.message.text;
 
-          if (prompt === "/start") {
-            ctx.reply("Нажмите на кнопку Open App");
+          if (prompt === '/start') {
+            ctx.reply('Нажмите на кнопку Open App');
 
-        bot
-          .launch()
-          .catch((e) =>
-            console.log(`Не удалось запустить телеграмм-бота`, e.message)
-          );
-      }
-    }
-  ]
+            bot
+              .launch()
+              .catch((e) =>
+                console.log(`Не удалось запустить телеграмм-бота`, e.message),
+              );
+          }
+        });
+      },
+    },
+  ],
 })
-export class AppModule {
-}
+export class AppModule {}
