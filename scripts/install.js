975b14dc3910a63091cbc23f1f4d6d64dd26531e
Maksim Zakharov <max89701@gmail.com>
2024-08-17 13:49:51 +0300

    cache brands

--- scripts/install.js
+++ scripts/install.js
@@ -1,4 +1,4 @@
 var cp = require('child_process');
-cp.exec('./install_puppeteer_deps.sh', function(err, stdout, stderr) {
+cp.exec('./scripts/install_puppeteer_deps.sh', function(err, stdout, stderr) {
   // handle err, stdout, stderr
 });
